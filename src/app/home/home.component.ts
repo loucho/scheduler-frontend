718d90e2880974a14bc03806b843c4e2314f7f31
loucho <loucho.advanced@gmail.com>
2022-02-20 22:50:50 +0900

    1 page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { finalize } from 'rxjs/operators';
 
-import { QuoteService } from './quote.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,19 +10,10 @@ export class HomeComponent implements OnInit {
   quote: string | undefined;
   isLoading = false;
 
-  constructor(private quoteService: QuoteService) {}
+  constructor() {}
 
   ngOnInit() {
     this.isLoading = true;
-    this.quoteService
-      .getRandomQuote({ category: 'dev' })
-      .pipe(
-        finalize(() => {
-          this.isLoading = false;
-        })
-      )
-      .subscribe((quote: string) => {
-        this.quote = quote;
-      });
+    this.isLoading = false;
   }
 }
